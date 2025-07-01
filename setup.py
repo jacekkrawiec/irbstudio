85db18504f29dd31db6aceb5a6bb796b8f684991
Jacek Krawiec <jacekkrawiec@gmail.com>
2025-07-01 22:21:00 +0200

    initial commit

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="irbstudio",
+    version="0.1.0",
+    description="Monitoring and validation toolkit for credit risk models under the IRB framework",
+    author="Jacek Krawiec",
+    author_email="jacekkrawiec@example.com", #to be replaced
+    url="https://github.com/yourusername/irbstudio", #to be replaced
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Intended Audience :: Financial and Insurance Industry",
+        "Topic :: Scientific/Engineering :: Information Analysis",
+    ],
+)
